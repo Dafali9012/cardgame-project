f9635b59b0317059517ebee13087c14e6956ef77
Daniel <Daniel.falklimberg@gmail.com>
2020-11-11 16:41:43 +0100

    creaturecard constructors

--- src/main/CreatureCard.java
+++ src/main/CreatureCard.java
@@ -3,9 +3,21 @@ package main;
 public class CreatureCard extends Card {
     private int maxHealth, health, defaultDamage, damage;
 
-    public CreatureCard() {}
+    public CreatureCard() {
+        super();
+        this.maxHealth = 1;
+        this.health = 1;
+        this.defaultDamage = 1;
+        this.damage = 1;
+    }
 
-    public CreatureCard(String title, String description, int maxHealth, int defaultDamage) {}
+    public CreatureCard(String title, String description, int maxHealth, int defaultDamage) {
+        super(title, description);
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.defaultDamage = defaultDamage;
+        this.damage = defaultDamage;
+    }
 
     public void modifyHealth(int amount) {}
 
