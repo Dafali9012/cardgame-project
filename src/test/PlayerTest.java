173fce8633d57feab1ed1b801160bd369f07f319
David Glane <david@glane.net>
2020-11-11 21:26:30 +0100

    create player test

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -1,7 +1,23 @@
 package test;
 
+import main.Player;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
+    private Player player;
 
+    public PlayerTest(){
+        addPlayerTest();
+    }
 
+    @Test
+    public void addPlayerTest(){
+
+        player = new Player("David", 5, 20);
+        assertEquals("David", player.getName());
+        assertEquals(5, player.getId());
+        assertEquals(20, player.getHealth());
+
+    }
 }
