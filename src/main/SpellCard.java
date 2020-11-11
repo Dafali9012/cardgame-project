45f17bcc47547f64c07ccdb93ec1e37fa84a78e4
Daniel <Daniel.falklimberg@gmail.com>
2020-11-11 16:38:09 +0100

    spellcard constructors

--- src/main/SpellCard.java
+++ src/main/SpellCard.java
@@ -3,9 +3,17 @@ package main;
 public class SpellCard extends Card {
     int damage, heal;
 
-    public SpellCard() {}
+    public SpellCard() {
+        super();
+        this.damage = 1;
+        this.heal = 1;
+    }
 
-    public SpellCard(String title, String description, int damage, int heal) {}
+    public SpellCard(String title, String description, int damage, int heal) {
+        super(title, description);
+        this.damage = damage;
+        this.heal = heal;
+    }
 
     public int getDamage() {
         return damage;
