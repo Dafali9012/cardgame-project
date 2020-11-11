e2c9641eb96a68c002151a6b434b86b8c64b91d3
Daniel <Daniel.falklimberg@gmail.com>
2020-11-11 16:14:02 +0100

    card constructors

--- src/main/Card.java
+++ src/main/Card.java
@@ -3,9 +3,15 @@ package main;
 public abstract class Card {
     private String title, description;
 
-    public Card() {}
+    public Card() {
+        this.title = "Undefined";
+        this.description = "Undefined";
+    }
 
-    public Card(String title, String description) {}
+    public Card(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 
     public String getTitle() {
         return title;
