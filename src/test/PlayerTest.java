620509126259081cffdbce22bbe22512d0f43fcb
David Glane <david@glane.net>
2020-11-12 12:08:28 +0100

    constructorWithoutArgument tester

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -5,17 +5,31 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
-    private Player player;
 
     public PlayerTest() {
-        constructorWithArgument();
     }
 
     @Test
     public void constructorWithArgument() {
-        player = new Player("David", 5, 20);
+
+        Player player = new Player("David", 5, 20);
         assertEquals("David", player.getName(), "Expected name 'David' got: " + player.getName());
         assertEquals(5, player.getId(), "Expected id '5' got: " + player.getId());
         assertEquals(20, player.getHealth(), "Expected health '20' got: " + player.getHealth());
+    }
+
+    @Test
+    public void constructorWithoutArgument() {
+        try {
+            new Player();
+        } catch (Exception ex) {
+            fail();
+        }
+
+        Player player = new Player();
+        assertEquals(null, player.getName());
+        assertEquals(0, player.getId());
+        assertEquals(0, player.getId());
+        assertEquals(0, player.getHealth());
     }
 }
