d8a7e2ad09c076907160f700eb53e456659a70c6
omanas98 <omanas_98@hotmail.com>
2020-11-12 14:37:00 +0100

    Tomma metoder, och test för turnCounter

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -1,26 +1,23 @@
 package test;
 
 import main.Game;
-import main.Player;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class GameTest {
-    public static void main(String[] args) {
 
-        //Test 1: Test if PlayerHealth gets to 1hp
-        var PlayerHealth = Game.reducePlayerHealth(100, 99);
-        assert (PlayerHealth < 1);
-        if (PlayerHealth < 1) {
-            throw new RuntimeException(
-            "Game.checkIfGameOverIsTrue - expected gameover to be true" +
-                    "and got: " + PlayerHealth);
-        }
+    Game g;
+    @Before
+    public void setupGame() {
+        g = new Game();
+    }
 
-        //Test 2: Test if PlayerHealth gets to 1hp
-        PlayerHealth = Game.reducePlayerHealth(100, 200);
-        if (PlayerHealth < 1) {
-            throw new RuntimeException(
-                "Game.checkIfGameOverIsTrue - expected gameover to be true" +
-                    "and got: " + PlayerHealth);
-        }
+    @Test
+    public void testTurnCounterInitialState() {
+        Assert.assertEquals("Expected turn count to be 1",1, g.runNextTurn());
     }
 }
