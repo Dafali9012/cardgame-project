40c18544fc13017072e13902425057615e0c541d
Anton <anton.ohlin@hotmail.com>
2020-11-16 15:34:34 +0100

    Reset main to prev

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,24 +5,22 @@ import static main.Utils.*;
 
 public class Main {
     public static void main(String[] args) {
-        InputHandler ih = new InputHandler();
-        ih.getInt("Choose a number");
-//        List<Card> deck = generateDeck(getCardSettings("res/cards"));
-//        int cardCount = 1;
-//        for(Card card : deck) {
-//            System.out.println(cardCount+". "+card.getTitle());
-//            System.out.println((cardCount<10?"   \"":"    \"")+card.getDescription()+"\"");
-//            switch (card.getClass().getSimpleName()) {
-//                case "CreatureCard" -> {
-//                    CreatureCard c = (CreatureCard) card;
-//                    System.out.println((cardCount<10?"   ":"    ")+"HP: "+c.getHealth()+" ATK: "+c.getDamage());
-//                }
-//                case "SpellCard" -> {
-//                    SpellCard c = (SpellCard) card;
-//                    System.out.println((cardCount<10?"   ":"    ")+"HEAL: "+c.getHeal()+" DAMAGE: "+c.getDamage());
-//                }
-//            }
-//            cardCount++;
-//        }
+        List<Card> deck = generateDeck(getCardSettings("res/cards"));
+        int cardCount = 1;
+        for(Card card : deck) {
+            System.out.println(cardCount+". "+card.getTitle());
+            System.out.println((cardCount<10?"   \"":"    \"")+card.getDescription()+"\"");
+            switch (card.getClass().getSimpleName()) {
+                case "CreatureCard" -> {
+                    CreatureCard c = (CreatureCard) card;
+                    System.out.println((cardCount<10?"   ":"    ")+"HP: "+c.getHealth()+" ATK: "+c.getDamage());
+                }
+                case "SpellCard" -> {
+                    SpellCard c = (SpellCard) card;
+                    System.out.println((cardCount<10?"   ":"    ")+"HEAL: "+c.getHeal()+" DAMAGE: "+c.getDamage());
+                }
+            }
+            cardCount++;
+        }
     }
 }
