51816bc5cdecf4c96d36b27b95a56daec327286e
razvan22 <razviy69@gmail.com>
2020-11-16 08:49:35 +0100

    dispenseHand

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -1,7 +1,12 @@
 package main;
 
-public class UserInterface {
+import java.lang.reflect.Array;
+import java.util.List;
 
+public class UserInterface {
+    private void askForUsername(){
+        System.out.println("Write username:");
+    }
     private void displayTurnOptions(){
     }
 
@@ -11,14 +16,33 @@ public class UserInterface {
     public void displayCreatureCard(){
     }
 
-    public String dispensePlayerNameAndHp(Player player){
+    public void displayHand(){
+    }
+
+    public String dispenseHand(List<CreatureCard> cards ){
         return null;
     }
 
+    public String dispensePlayerNameAndHp(Player player){
+        return null;
+    }
 
     public String dispenseCreatureCard(CreatureCard card){
         return null;
     }
 
 
+}
+
+class Test{
+    public static void main(String[] args) {
+        Player player1 = new Player("Jon",1,100);
+        Player player2 = new Player("Tim",2,100);
+        Player[] players = new Player[2];
+        players[0] = player1;
+        players[1] = player2;
+
+        System.out.println(players[0].getName());
+
+    }
 }
--- src/test/TestUserInterface.java
+++ src/test/TestUserInterface.java
@@ -6,10 +6,13 @@ import main.UserInterface;
 import org.junit.Test;
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+
 public class TestUserInterface {
     UserInterface userInterface = new UserInterface();
-    Player player = new Player("Player1",100);
+    Player player = new Player("Player1",1,100);
     CreatureCard card = new CreatureCard();
+    ArrayList<CreatureCard> cards = new ArrayList<CreatureCard>();
 
     @Test
    public void testDispensePlayerNameAndHp() {
@@ -25,5 +28,12 @@ public class TestUserInterface {
         Assertions.assertTrue(  userInterface.dispenseCreatureCard(card) instanceof java.lang.String );
     }
 
+    @Test
+
+    public  void dispenseHand(){
+        System.out.println("======TEST ONE dispenseHand");
+        Assertions.assertNotNull(userInterface.dispenseHand(cards));
+
+    }
 
 }
