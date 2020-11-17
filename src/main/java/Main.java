bfca0e98f55856e25f9ef96e72bb4406782eb258
David Glane <david@glane.net>
2020-11-17 11:06:58 +0100

    implement travis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         UserInterface userInterface  = new UserInterface();
-        List<Card> deck = generateDeck(getCardSettings("res/cards"));
+        List<Card> deck = Utils.generateDeck(Utils.getCardSettings("res/cards"));
         List<Card> cards = new ArrayList<>();
 
         int cardCount = 1;
