3d76eb852f637284763573dc590fe0332debbe5b
omanas98 <omanas_98@hotmail.com>
2020-11-18 14:26:37 +0100

    isGameOver metoden work in progress färdigställd, getcurrentPlayersTurn färdigställd

--- src/main/Game.java
+++ src/main/Game.java
@@ -23,20 +23,34 @@ public class Game {
     }
 
     /**
-     * Get player that has the current turn.
-     * @return player1 or player2.
-     */
-    private Player getCurrentPlayersTurn(){
-        return null;
-    }
-
-    /**
      * Check if game is over.
      * @return returns true if either player has 0 health.
      */
     public boolean isGameOver(){
         return true;
+        if(player1.getHealth() < 1) {
+            System.out.println("Game is over");
+            return true;
+        } else {
+            runNextTurn();
+            return false;
+        }
     }
+
+    /**
+     * Get current turn
+     * @return what player is playing
+     */
+    private Player getCurrentPlayersTurn(){
+            if(turnCounter%2 == 0){
+                System.out.println("It's your turn");
+                return player1;
+            } else{
+                System.out.println("It's The enemies turn");
+                return player2;
+            }
+        }
+
 
     /**
      * Plays next turn.
