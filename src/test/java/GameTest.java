964063fa27bf1fca9111c16f891f3df0807c70dc
David Glane <david@glane.net>
2020-11-19 17:20:50 +0100

    drawCardTest

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -9,12 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
     List<Card> deck;
     List<Card> to;
+    Player player;
     Game game;
 
     @BeforeEach
     public void beforeTest(){
         deck = Cards.generateDeck(Cards.getCardTemplates("res/card-templates"));
         to = Cards.generateDeck(Cards.getCardTemplates("res/card-templates"));
+        player = new Player("Daniel", 5, 100);
+        game = new Game();
     }
 
     @Test
@@ -22,6 +25,20 @@ public class GameTest {
         assertTrue(game.moveCard(1, deck, to), "Moving card not successful");
         assertFalse(game.moveCard(-1, deck, to), "Index cannot be negative");
         assertFalse(game.moveCard(100, deck, to), "Index cannot be longer than the list itself");
+    }
+
+    @Test
+    public void drawCardTest () {
+        Card obj = to.get(0);
+        to.clear();
+        to.add(obj);
+        player.setDeck(to);
+        player.setHand(to);
+        assertTrue(game.drawCard(player));
+
+        player.setDeck(deck);
+        player.setHand(deck);
+        assertTrue(game.drawCard(player));
     }
 
 }
