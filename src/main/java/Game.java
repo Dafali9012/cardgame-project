b7ccd53fcf28a6afcb25165601362dd8391ef1dd
David Glane <david@glane.net>
2020-11-24 12:19:58 +0100

    whenReadyMsg

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -65,7 +65,7 @@ public class Game {
     public void playerChangeScreen() {
         if(turnCount!=1) Screen.printSpacing(100);
         Screen.printPlayerStart(player,turnCount);
-        Input.text("Enter nothing to continue: ", new String[]{""});
+        Input.text("Press enter when ready -> ", new String[]{""});
         Screen.printSpacing(100);
     }
 
